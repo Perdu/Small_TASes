d9a6422ad1c41eb7ecb83f6b3469178ee1625d45
Célestin Matte <dev@cmatte.me>
2026-02-10 15:36:15 +0100

    gridgame: bruteforcer: fix timeout

--- Gridgame/bruteforcer.sh
+++ Gridgame/bruteforcer.sh
@@ -2,5 +2,4 @@
 
 # One-liner I used inside my libTAS docker container
 
-
-i=0 ; while true; do i=$((i+1)) ; echo "Now doing seed $i" ; timeout 10m xvfb-run -a libTAS -i -n -t "$i" -L -r /home/gridgame.ltm --lua lua/gridgame.lua /usr/local/bin/ruffle_desktop -g gl --no-gui /home/gridgame.swf ; done
+i=0 ; while true; do i=$((i+1)) ; echo "Now doing seed $i" ; timeout 720 xvfb-run -a libTAS -i -n -t "$i" -L -r /home/gridgame.ltm --lua lua/gridgame.lua /usr/local/bin/ruffle_desktop -g gl --no-gui /home/gridgame.swf ; done
